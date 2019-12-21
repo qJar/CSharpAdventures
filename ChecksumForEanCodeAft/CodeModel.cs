b1948c532951e70ebac692547790cfa9fd904341
Jarek Szpond <jarek.szpond@hotmail.com>
2019-12-21 21:26:14 +0100

    Added CodeModel class

---
+++ ChecksumForEanCodeAft/CodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChecksumForEanCodeAft
+{
+    public class CodeModel
+    {
+        public string Code { get; set; }
+        public bool IsLengthValid { get; set; }
+        public bool IsCharValid { get; set; }
+    }
+}
