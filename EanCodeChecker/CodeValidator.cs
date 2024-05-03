2e15c04f767b4bfe0319feffdecf624c910d3c56
Jarek <jarek.szpond@hotmail.com>
2024-05-04 01:51:32 +0200

    New Project has been added

---
+++ EanCodeChecker/CodeValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EanCodeChecker
+{
+    /// <summary>Typ wyliczeniowy kodow EAN z przypisana dlugoscia znakow
+    /// </summary>
+    public enum EanCodeType
+    {
+        EAN8 = 8,
+        EAN13 = 13
+    }
+
+    public class CodeValidator
+    {
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+    }
+}
